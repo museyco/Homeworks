6100176b715c944da3081b31569017ccb0017e49
Musa Can Haliloglu <98672276+museyco@users.noreply.github.com>
2023-05-23 17:27:26 +0300

    digitTotal

---
+++ digitTotal/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int num,mainNum,digit,total;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Sayıyı Giriniz: ");
+        num = inp.nextInt();
+
+        total = 0;
+        while(num != 0){
+            digit = num % 10;
+            System.out.println("  "+digit);
+            total += digit;
+            num /= 10;
+        }
+        System.out.println("+");
+        System.out.println("----------------------");
+        System.out.println("Sonuç: "+total);
+
+
+    }
+}
